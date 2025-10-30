1a7a75ff346b52199036eb828be998eef7932cd8
dasha2512 <safronovadara998@gmail.com>
2025-10-30 18:52:41 +0300

    new commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Моя страница </title>
+		<link rel="stylesheet" href='mycss.css'>
+		<link rel="shortcut icon" href='favicon.ico'>
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Результат вычисления объема цилиндра с площадью основания 6 и высотой 12 равен ";
+				echo 6*12;
+			?>
+		</div>
+	</body>
+</html>
